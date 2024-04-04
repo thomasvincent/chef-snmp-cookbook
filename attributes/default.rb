2f820c124a8cef625675025072b87b5f6d4feda4
Thomas Vincent <thomasvincent@gmail.com>
2024-04-04 16:42:57 -0700

    Refactor SNMP attributes for DRY
    This commit streamlines the SNMP cookbook attributes by reducing repetition, optimizing the use of data structures, and making platform-specific adjustments more concise. Changes include:
    
    - **Setting default SNMP package names and service name**, overriding them only for specific platforms where necessary.
    - **Replacing string keys with symbols in hashes** for better memory efficiency.
    - **Consolidating repeated values** and leveraging Ruby’s `case` statement to apply platform-specific adjustments in a cleaner way.
    - **Adding comments** for better readability and maintainability of the code.
    
    These improvements aim to make the SNMP cookbook attributes easier to understand and maintain, while also optimizing performance where possible.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,7 @@
 # Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+#xq
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
@@ -16,43 +16,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['snmp']['packages'] =
-  case node['platform_family']
-  when 'rhel', 'fedora', 'amazon'
-    %w(net-snmp net-snmp-utils)
-  when 'debian', 'ubuntu'
-    %w(snmp snmpd)
-  when 'suse', 'opensuse'
-    %w(net-snmp)
-  when 'freebsd'
-    %w(net-snmp)
-  when 'mac_os_x', 'mac_os_x_server'
-    %w(net-snmp)
-  when 'solaris2'
-    %w(SUNWucsnmp)
-  when 'aix'
-    %w(net-snmp)
-  else
-    %w(net-snmp net-snmp-utils)
-  end
+# SNMP Cookbook Default Attributes
+# Sets default packages, services, and configurations for SNMP based on platform
 
-default['snmp']['service'] =
-  case node['platform_family']
-  when 'rhel', 'fedora', 'amazon', 'debian', 'ubuntu', 'suse', 'opensuse', 'freebsd', 'mac_os_x', 'mac_os_x_server', 'aix'
-    'snmpd'
-  when 'solaris2'
-    'svc:/network/snmp/dmi:default'
-  end
+# Default packages and service names, applicable to most platforms
+default['snmp']['packages'] = %w(net-snmp net-snmp-utils)
+default['snmp']['service'] = 'snmpd'
 
+# Platform-specific adjustments
+case node['platform_family']
+when 'rhel', 'fedora', 'amazon', 'suse', 'opensuse', 'freebsd', 'mac_os_x', 'mac_os_x_server', 'aix'
+  # No changes needed, defaults apply
+when 'debian', 'ubuntu'
+  default['snmp']['packages'] = %w(snmp snmpd)
+when 'solaris2'
+  default['snmp']['packages'] = %w(SUNWucsnmp)
+  default['snmp']['service'] = 'svc:/network/snmp/dmi:default'
+end
+
+# Default SNMP community and security names
 default['snmp']['community'] = 'public'
-default['snmp']['sec_name'] = { 'notConfigUser' => %w(default) }
-default['snmp']['sec_name6'] = { 'notConfigUser' => %w(default) }
+default['snmp']['sec_name'] = { notConfigUser: %w(default) }
+default['snmp']['sec_name6'] = { notConfigUser: %w(default) }
 
+# Default SNMP groups
 default['snmp']['groups'] = {
-  'v1' => { 'notConfigGroup' => %w(notConfigUser) },
-  'v2c' => { 'notConfigGroup' => %w(notConfigUser) }
+  v1: { notConfigGroup: %w(notConfigUser) },
+  v2c: { notConfigGroup: %w(notConfigUser) }
 }
 
+# Default SNMP trap configurations
 default['snmp']['trap']['community'] = 'public'
 default['snmp']['trap']['addresses'] = []
 default['snmp']['trap']['port'] = 162
+
